2d511312e73855bf7b923d08b0bafddca79ae862
Cynthia Salazar <cinthya3259@gmail.com>
2021-02-15 10:07:52 -0600

    Final1

--- 17_sum_fac.sql
+++ 17_sum_fac.sql
@@ -1,5 +1,4 @@
-SELECT SUM(subtotal) 
-FROM factura;
-
-SELECT SUM(total)
-FROM factura;
+SELECT d.number_factura AS number_factura, SUM(d.unit_price * d.quantity_product)
+AS subtotal, SUM(d.unit_price * d.quantity_product * 0.13)
+AS iva, SUM(d.unit_price * d.quantity_product * 0.13) + SUM(d.unit_price * d.quantity_product) AS 
+precio_total FROM factura_product AS d GROUP BY d.number_factura
